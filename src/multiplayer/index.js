64ea52415caee06fab4158f6a928ee83dd6644f3
Alex Ilushyk <ailushyk@gmail.com>
2022-08-13 14:27:08 +0200

    multiplayer

--- src/multiplayer/index.js
+++ src/multiplayer/index.js
@@ -1,12 +1,42 @@
+export const DEMO_TABLE = 'table-1'
+
 export const multiplayer = (io, socket) => {
-  const ping = async () => {
-    socket.volatile.emit('pong')
+  socket.join(DEMO_TABLE)
+  const result = async () => {
+    const sockets = await io.fetchSockets()
+    const players = sockets
+      .filter((socket) => !!socket.player)
+      .map((socket) => {
+        return socket.player
+      })
+    // to all clients in room "DEMO_TABLE"
+    io.in(DEMO_TABLE).emit('multiplayer:result', players)
   }
 
-  const pong = () => {
-    console.log(`pong ${Date.now()}`)
+  const join = async (username) => {
+    socket.player = {
+      username,
+      sp: null,
+    }
+    await result()
   }
 
-  socket.on('ping', ping)
-  socket.on('pong', pong)
+  const vote = async (payload) => {
+    socket.player = payload
+    await result()
+  }
+
+  const check = async (value) => {
+    io.in(DEMO_TABLE).emit('multiplayer:check', value)
+    await result()
+  }
+
+  const disconnect = async () => {
+    await result()
+  }
+
+  socket.on('multiplayer:join', join)
+  socket.on('multiplayer:vote', vote)
+  socket.on('multiplayer:check', check)
+  socket.on('disconnect', disconnect)
 }
